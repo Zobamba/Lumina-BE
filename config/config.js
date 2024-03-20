f57d1c490b8ad12e9df68a76f2368f5983b5271a
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-03-20 23:25:09 +0100

    connected to production database

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ module.exports = {
   "development": {
     "username": "chizoba",
     "password": "onah2020",
-    "database": "Lumina",
+    "database": "lumina",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
     "username": "chizoba",
     "password": "onah2020",
-    "database": "Lumina",
+    "database": "lumina",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },

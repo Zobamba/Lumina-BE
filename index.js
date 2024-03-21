e7252a788a7f927ba2613c0503f5c069381b6bdc
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-03-21 09:45:31 +0100

    config.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['https://lumina-nzfj.vercel.app'],
+  origin: ['http://localhost:5173', 'https://lumina-nzfj.vercel.app'],
   credentials: true,
 };
 
